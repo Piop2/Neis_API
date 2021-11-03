1b0a23e8989f36a744d575074a4cc052a9e9e127
Piop2 <seungyounyou0220@gmail.com>
2021-11-03 21:17:01 +0900

    add exceptions.py

---
+++ neis_api/exceptions.py
@@ -0,0 +1,7 @@
+class IncorrectParamError(Exception):
+    def __init__(self):
+        super().__init__("필수 값이 누락되어 있습니다. 요청인자를 참고 하십시오.")
+
+class InvalidKeyError(Exception):
+    def __init__(self):
+        super().__init__("인증키가 유효하지 않습니다. 인증키가 없는 경우, 홈페이지에서 인증키를 신청하십시오.")
