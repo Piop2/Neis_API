1d6139307e914ee2132b230358fc9dc46a13486b
Piop2 <seungyounyou0220@gmail.com>
2022-04-06 12:48:37 +0900

    __init__.py 오타 수정

--- Neis_API/__init__.py
+++ Neis_API/__init__.py
@@ -2,14 +2,14 @@ import urllib3
 
 urllib3.disable_warnings()
 
-from .region import *
+from Neis_API.region import *
 
 import Neis_API.service
 
 get_meal = Neis_API.service.schoolMeal.get_meal_data
 get_school = Neis_API.service.schoolInfo.get_school_data
-get_schedule = Neis_API.service.schoolschedule.get_schedule_data
+get_schedule = Neis_API.service.schoolSchedule.get_schedule_data
 
 import Neis_API.school
 
-School = Neis_API.School
+School = Neis_API.school.School
