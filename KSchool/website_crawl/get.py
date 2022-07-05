242f3a5fa46f973c93bf12d4e8643592b1733e1c
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-07-05 23:13:28 +0900

    중간저장

--- KSchool/website_crawl/get.py
+++ KSchool/website_crawl/get.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 
 
 def crawl_website(url: str) -> BeautifulSoup:
+    """
+    웹사이트를 크롤링하는 함수
+
+    :param url: 웹사이트 링크
+    :return: :class: `bs4.BeautifulSoup` object
+    """
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
         html = requests.get(str(url), headers=headers, allow_redirects=True)
@@ -16,7 +22,14 @@ def crawl_website(url: str) -> BeautifulSoup:
         content = BeautifulSoup(html, "html.parser")
     return content
 
+
 def get_school_website_link(school_name: str) -> str:
+    """
+    학교 사이트의 링크를 구글 검색을 통해 받아오는 함수
+
+    :param school_name: 학교 이름
+    :return: str
+    """
     school_link = ""
     limit = 10
     while limit >= 0:
